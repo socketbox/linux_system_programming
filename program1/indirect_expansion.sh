1a4410f25ceab7879935c17f11e34a4677abbeec
Casey Boettcher <chb@socketbox.com>
2019-10-09 10:08:49 -0400

    Almost done with single file input commands

--- program1/indirect_expansion.sh
+++ program1/indirect_expansion.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
-INFILE="infile$$"
-echo "$INFILE: $INFILE"
-echo INFILE quoted "$INFILE"
 
-catinto()
-{
-  cat > $INFILE
-  cat $INFILE
-  if [ -r $INFILE ]
-  then
-    echo -e "Reading is good!"
-  fi
-}
+#INFILE="infile$$"
+#echo "$INFILE: $INFILE"
+#echo INFILE quoted "$INFILE"
+#
+#catinto()
+#{
+#  cat > $INFILE
+#  cat $INFILE
+#  if [ -r $INFILE ]
+#  then
+#    echo -e "Reading is good!"
+#  fi
+#}
+#
+#vartest()
+#{
+#  cat $INFILE
+#  if [ -r $INFILE ]
+#  then
+#    echo -e "Reading is good!"
+#  else
+#    echo -e "Readins is bad"
+#  fi
+#}
 
-vartest()
-{
-  cat $INFILE
-  if [ -r $INFILE ]
-  then
-    echo -e "Reading is good!"
-  else
-    echo -e "Readins is bad"
-  fi
-}
-
-#i=0
-#while IFS=$'\n' read -r line
-#do
-#  readarray -t -d$'\t' rowarr$i <<< "$line"; declare -p rowarr$i;
-#  ((i++))
-#done < "$1"
+i=0
+while IFS=$'\n' read -r line
+do
+  #readarray -d$'\t' -t rowarr$i <<< "$line"; declare -p "rowarr$i";
+  readarray -d$'\t' -t rowarr$i < <(printf "%s" "$line") 
+  declare -p "rowarr$i";
+  ((i++))
+done < "$1"
 #
 # This works:
-#echo "First row: ${rowarr0[@]}"
-#k=4
+echo "First row: ${rowarr0[@]}"
+k=4
 #var="rowarr$t[@]"
 #echo "Row five: ${!var}"
-#c=4
-#var="rowarr$k[$c]"
-#echo "Row five, element 5: ${!var}"
+c=4
+var="rowarr$k[$c]"
+echo "Row five, element 5: ${!var}"
 
-#var="rowarr$k[$c]"
-#for (( c = 0; c < 7; c++ ))
-#do
-#  for ((k = 0; k < 9; k++))
-#  do
-#    var="rowarr$k[$c]"
-#    echo "This is c: $c"
-#    echo "This is k: $k"
-#    echo "this is var: $var" 
-#    echo "First row: ${rowarr0[@]}"
-#    echo "${!var}"
+for (( c = 0; c < 4; c++ ))
+do
+  for ((k = 0; k < 3; k++))
+  do
+    var="rowarr$k[$c]"
+    echo "This is c: $c"
+    echo "This is k: $k"
+    rowvar="rowarr$k[@]"
+    echo "this is row: ${!rowvar}" 
+    echo "this is var: ${!var}" 
     #echo -e "${rowarr$k}"
     #echo -e "$rowarr${k}"
     #echo -e $rowarr${k}
@@ -77,10 +79,10 @@ vartest()
       #echo ${!row[$c]}
   #    idx=${c} 
   #    echo -e ${!row} #[${idx}]}
-#  done
-#done
+  done
+done
 
 #echo "colsum: $colsum"  
 
-catinto
-vartest
+#catinto
+#vartest
