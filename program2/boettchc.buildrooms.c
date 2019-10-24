0132927c2f9cf15339a317a2b7a839489d555a14
Casey Boettcher <chb@socketbox.com>
2019-10-23 23:44:49 -0700

    mk_rm_dir working

--- program2/boettchc.buildrooms.c
+++ program2/boettchc.buildrooms.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-#define ONID boettchc
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#define ROOM_CNT 7
+#define ONID "boettchc\0"
+#define ROOMS "rooms\0"
 
 enum rm_type { START_ROOM, MID_ROOM, END_ROOM };
 
-typedef struct Room
+typedef struct room
 {
   char *rm_name;
   int rm_number;
@@ -15,30 +23,6 @@ typedef struct Room
   int cx_count;
   unsigned char cxs;
 } Room;
-
-
-/* no exponent operator in C? No pow() variant that takes and returns an int?
- * Laaaamme.
- * pre:   n/a
- * in:    two unsigned ints, a base and an exponent
- * out:   an int
- * post:  n/a
- */
-int powi(unsigned int base, unsigned int exp)
-{
-  int res = 0;
-  if(exp == 0)
-    res = 1;
-  else
-  {
-    res = base;
-    for(int n = 1; n < exp; n++)
-    {
-      res *= base;
-    }
-  }
-  return res;
-}
 
 
 /*
@@ -56,6 +40,93 @@ int get_rand( unsigned int low, unsigned int high)
   r = low + ( r / ( (RAND_MAX + 1u) / (high + 1) ) );
   return r;
 }
+
+
+/* 
+ * calculates int value given base and exponent
+ *
+ * pre:   n/a
+ * in:    two unsigned ints, a base and an exponent
+ * out:   an int
+ * post:  n/a
+ */
+int powi(unsigned int base, unsigned int exp)
+{
+  int res = 0;
+  if(exp == 0)
+    res = 1;
+  else
+  {
+    int n;
+    res = base;
+    for(n = 1; n < exp; n++)
+    {
+      res *= base;
+    }
+  }
+  return res;
+}
+
+
+/* 
+ * creates the room directory
+ *
+ * pre:     n/a
+ * in:      pid of current process
+ * out:     n/a
+ * post:    room directory exists in cwd
+ */
+void mk_rm_dir(pid_t pid)
+{
+  char *delim = ".";
+  /* cat /proc/sys/kernel/pid_max returns 49152 on os1 */
+  int pid_str_buff_sz = 5 * sizeof(char); 
+  char pid_str[pid_str_buff_sz]; 
+  sprintf(pid_str, "%i", pid);
+  int dir_name_len = strlen(ROOMS) + strlen(ONID) + pid_str_buff_sz + 2;
+  char dir_name[dir_name_len];
+  memset(dir_name, '\0', sizeof(char)*dir_name_len);
+  strcpy(dir_name, ONID);
+  strcat(dir_name, delim);
+  strcat(dir_name, ROOMS);
+  strcat(dir_name, delim);
+  strcat(dir_name, pid_str);
+  if(DEBUG)
+    printf("room dir name: %s\n", dir_name);
+}
+
+
+char* get_rm_name(char *names[])
+{
+  int idx = get_rand(0, sizeof(names));
+  char *n = names[idx];
+  if(DEBUG)
+    printf("get room name: %s" n);
+  return n;
+}
+
+
+Room* gen_rooms(int count, char *names[])
+{
+  Room *rooms = malloc(count*sizeof(Room));
+
+  char *name = NULL;
+
+  for(int i = 0; i < count; i++)
+  {
+    if(i = 0)
+      type = START_ROOM;
+    else if (i = 1)
+      type = END_ROOM;
+    else
+      type = MID_ROOM;
+
+    name = get_rm_name(names);       
+  }
+
+  return rooms;
+}
+
 
 
 /*
@@ -79,15 +150,23 @@ int are_connected(Room rm1, Room rm2)
 
 int main( int argc, char **args )
 {
-  //seed the PRNG
-  srand(time(NULL));
-
-  // Declare and init an array of ten room labels
+  /* Declare and init an array of ten room labels */
   char *ten_rooms[] = {"BC", "Alberta", "Saskatch", "Manitoba", "Ontario", "Quebec", "NewfLab", "NewBrun", "NovaScot", "PrinceEd"};
-  
-  // # make an $ONID.rooms.$PID directory
-  
+ 
+  /* get the PID */
+  pid_t pid = getpid();
 
+  /* make room file directory */
+  mk_rm_dir(pid);
+
+  /* seed the PRNG */
+  srand(time(NULL));
+  
+  /* make an $ONID.rooms.$PID directory */
+  Room *rooms = gen_rooms(ROOM_CNT);  
+
+  free(rooms);
+  rooms = NULL;
   return 0;
 }
 
